9e0d49bb957b970ede20d356d0a66f206b337d87
eugene <support@bytescout.com>
2016-03-28 16:40:32 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿//*******************************************************************
+//       Visual Studio Versions Project Converter		                                     
+//                                                                   
+//       Copyright © 2016 ByteScout - http://www.bytescout.com       
+//       ALL RIGHTS RESERVED                                         
+//                                                                   
+//*******************************************************************
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VSProjectConverter2
+{
+	public enum VisualStudioVersion
+	{
+		VS2005, VS2008, VS2010, VS2012, VS2013, VS2015,
+	}
+
+	class Program
+	{
+		public static string InputFile = null;
+		public static List<VisualStudioVersion> TargetVersions = new List<VisualStudioVersion>();
+		public static bool RenameCurrent = false;
+
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				PrintUsage();
+				
+				return;
+			}
+
+			foreach (string arg in args)
+			{
+				if (File.Exists(arg))
+				{
+					InputFile = Path.GetFullPath(arg);
+
+					string extension = Path.GetExtension(InputFile);
+					if (extension != ".csproj" && extension != ".vbproj")
+					{
+						Console.WriteLine("Unsupported project file type");
+						return;
+					}
+				}
+				else if (arg == "/rename")
+				{
+					RenameCurrent = true;
+				}
+				else if (arg == "/allversions")
+				{
+					TargetVersions.AddRange(Enum.GetValues(typeof(VisualStudioVersion)).Cast<VisualStudioVersion>());
+				}
+				else if (arg == "/vs2005")
+				{
+					if (!TargetVersions.Contains(VisualStudioVersion.VS2005))
+						TargetVersions.Add(VisualStudioVersion.VS2005);
+				}
+				else if (arg == "/vs2008")
+				{
+					if (!TargetVersions.Contains(VisualStudioVersion.VS2008))
+						TargetVersions.Add(VisualStudioVersion.VS2008);
+				}
+				else if (arg == "/vs2010")
+				{
+					if (!TargetVersions.Contains(VisualStudioVersion.VS2010))
+						TargetVersions.Add(VisualStudioVersion.VS2010);
+				}
+				else if (arg == "/vs2012")
+				{
+					if (!TargetVersions.Contains(VisualStudioVersion.VS2012))
+						TargetVersions.Add(VisualStudioVersion.VS2012);
+				}
+				else if (arg == "/vs2013")
+				{
+					if (!TargetVersions.Contains(VisualStudioVersion.VS2013))
+						TargetVersions.Add(VisualStudioVersion.VS2013);
+				}
+				else if (arg == "/vs2015")
+				{
+					if (!TargetVersions.Contains(VisualStudioVersion.VS2015))
+						TargetVersions.Add(VisualStudioVersion.VS2015);
+				}
+			}
+
+			// default is /allversions
+			if (TargetVersions.Count == 0)
+				TargetVersions.AddRange(Enum.GetValues(typeof(VisualStudioVersion)).Cast<VisualStudioVersion>());
+
+			if (!String.IsNullOrEmpty(InputFile) && TargetVersions.Count > 0)
+			{
+				try
+				{
+					using (Converter converter = new Converter(InputFile, TargetVersions, RenameCurrent))
+					{
+						converter.Go();
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+					Console.WriteLine("Press any key...");
+					Console.ReadKey();
+				}
+			}
+		}
+
+		private static void PrintUsage()
+		{
+			Console.WriteLine("Upgrades or downgrades C# and VB project files to all Visual Studio versions.");
+			Console.WriteLine();
+			Console.WriteLine("VSProjectConverter2 filename [/addpostfix] [/allversions] [/vs2005] [/vs2008] [/vs2010] [/vs2012] [/vs2013] [/vs2015]");
+			Console.WriteLine();
+			Console.WriteLine("  /rename       Add \".VS20**\" postfix to input filename, e.g. \"MyProject.VS2010.csproj\".");
+			Console.WriteLine("  /allversions  (default) Create projects for all Visual Studio versions.");
+			Console.WriteLine("  /vs2005       Create Visual Studio 2005 compatible project.");
+			Console.WriteLine("  /vs2008       Create Visual Studio 2008 compatible project.");
+			Console.WriteLine("  /vs2010       Create Visual Studio 2010 compatible project.");
+			Console.WriteLine("  /vs2012       Create Visual Studio 2012 compatible project.");
+			Console.WriteLine("  /vs2013       Create Visual Studio 2013 compatible project.");
+			Console.WriteLine("  /vs2015       Create Visual Studio 2015 compatible project.");
+			Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿//*******************************************************************
+//       Visual Studio Versions Project Converter		                                     
+//                                                                   
+//       Copyright © 2016 ByteScout - http://www.bytescout.com       
+//       ALL RIGHTS RESERVED                                         
+//                                                                   
+//*******************************************************************
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VSProjectConverter2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VSProjectConverter2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f01bc142-676c-40f4-9bc1-f2fb5a251ec0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
